f803fc000fa163a2025665a75345324ae43f3ad1
Andrey Rezende <dreyrezende@hotmail.com>
2024-05-28 10:41:02 -0300

    improving log in FlutterSmsRetriever

--- android/src/main/kotlin/com/otp/autofill/otp_autofill_package/FlutterSmsRetriever.kt
+++ android/src/main/kotlin/com/otp/autofill/otp_autofill_package/FlutterSmsRetriever.kt
@@ -2,7 +2,6 @@ package com.otp.autofill.otp_autofill_package
 
 import android.app.Activity
 import android.content.BroadcastReceiver
-import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
@@ -21,9 +20,9 @@ class FlutterSmsRetriever(
 ) : BroadcastReceiver(), OtpReceiver {
 
     private val intentFilter = IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION)
-
+    private val tag = FlutterSmsRetriever::class.qualifiedName
     override fun dispose() {
-        Log.w(TAG, "FlutterSmsRetriever dispose")
+        Log.w(tag, "dispose")
         context.unregisterReceiver(this)
     }
 
@@ -31,42 +30,38 @@ class FlutterSmsRetriever(
         val client = SmsRetriever.getClient(activity)
         val task = client.startSmsRetriever()
         task.addOnSuccessListener {
-            Log.w(TAG, "FlutterSmsRetriever addOnSuccessListener")
+            Log.w(tag, "Waiting for SMS")
             context.registerReceiver(this, intentFilter)
         }
         task.addOnFailureListener {
-            Log.w(TAG, "FlutterSmsRetriever addOnFailureListener")
+            Log.w(tag, "Waiting for SMS failed")
         }
     }
 
     private fun emitOtp(otp: String?) {
-        Log.w(TAG, "otp emitted")
         if (otp != null) {
+            Log.w(tag, "Otp emitted $otp")
             onOtpReceived(otp)
         }
     }
 
     override fun onReceive(context: Context, intent: Intent) {
-        Log.w(TAG, "onReceive")
         if (SmsRetriever.SMS_RETRIEVED_ACTION == intent.action) {
             val extras: Bundle? = intent.extras
             val status: Status? = extras?.parcelable(SmsRetriever.EXTRA_STATUS)
-            Log.w(TAG, "SmsRetriever.SMS_RETRIEVED_ACTION")
-
             when (status?.statusCode) {
                 CommonStatusCodes.SUCCESS -> {
-                    Log.w(TAG, "CommonStatusCodes.SUCCESS")
                     val message = extras.getString(SmsRetriever.EXTRA_SMS_MESSAGE)
                     val otp = retrieveOtpFromMessage(message)
                     emitOtp(otp)
                 }
 
                 CommonStatusCodes.TIMEOUT -> {
-                    Log.w(TAG, "timeout")
+                    Log.w(tag, "Waiting for SMS timed out (5 minutes)")
                 }
 
                 else -> {
-                    Log.w(TAG, "unexpected error")
+                    Log.w(tag, "onReceive unexpected error")
                 }
             }
         }
